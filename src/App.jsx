44857c386fb30589cdd907201b8fcb0b072016ac
Dika Wicaksono <dhkwck00@gmail.com>
2024-04-26 23:03:58 +0700

    modify App.jsx file, add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <p>Ini Dashboard User</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
