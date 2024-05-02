b49a05f400dbdeb45c4a4fa7e19321259f659902
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-02 11:16:08 +0700

    changing routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import "./index.css";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    setIsAuthenticated(!!accessToken);
+  }, []);
+
+  const Private = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
+  const Auth = ({ element }) => {
+    return isAuthenticated ? <Navigate to="/" /> : element;
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<Auth element={<SignUp />} />} />
+        <Route path="/login" element={<Auth element={<Login />} />} />
       </Routes>
     </Router>
   );
