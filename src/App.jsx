9ef567961f81d18259493d6adc0f45f8ddd29d96
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-29 23:29:27 +0700

    refactor some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
-import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login, SignUp, Dashboard } from "./pages";
+import BaseLayout from "./layout/BaseLayout";
 import "./index.css";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    setIsAuthenticated(!!accessToken);
-  }, []);
-
-  const Private = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />;
-  };
-
-  const Auth = ({ element }) => {
-    return isAuthenticated ? <Navigate to="/" /> : element;
-  };
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/sign-up" element={<Auth element={<SignUp />} />} />
-        <Route path="/login" element={<Auth element={<Login />} />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<BaseLayout />}>
+          <Route path="/home" element={<Dashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
