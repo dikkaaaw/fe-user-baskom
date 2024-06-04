fe353f488b9cde60336473a00987468852c5db8b
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-04 15:05:32 +0700

    add aliases for not found page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as Login } from "./auth/Login";
 export { default as SignUp } from "./auth/SignUp";
 export { default as Dashboard } from "./home/Dashboard";
+export { default as NotFound } from "./error/NotFound";
