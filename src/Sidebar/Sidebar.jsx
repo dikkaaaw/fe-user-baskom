534b71bc6e0aba8cc672e1896ea7a59fad9f1315
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-04 08:36:04 +0700

    create sidebar component and styling it

---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import imgLogo from "../assets/img-logo-2.png";
+import "./Sidebar.css";
+
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <Link to="/">
+            <img src={imgLogo} alt="logo" className="logo" />
+          </Link>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
+
+Sidebar.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default Sidebar;
