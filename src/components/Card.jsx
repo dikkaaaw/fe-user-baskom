8197fa47ac8a60defeaa8fb111d7cb673628e9ec
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-04 08:33:13 +0700

    create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,38 @@
+import { PropTypes } from "prop-types";
+import { BsFillBagFill } from "react-icons/bs";
+
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
+  return (
+    <>
+      <section className="card">
+        <img src={img} alt={title} className="card-img" />
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            {star} {star} {star} {star}
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>{prevPrice}</del> {newPrice}
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+Card.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  star: PropTypes.node.isRequired,
+  reviews: PropTypes.string.isRequired,
+  prevPrice: PropTypes.string.isRequired,
+  newPrice: PropTypes.string.isRequired,
+};
+
+export default Card;
