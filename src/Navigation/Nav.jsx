80672d244ef9c5a372f50aa6962f01a806444d17
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-04 15:05:12 +0700

    create profile dropdown and styling it

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import { PropTypes } from "prop-types";
 import { AiOutlineUser } from "react-icons/ai";
 import "./Nav.css";
 
 const Nav = ({ handleInputChange, query }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleDropdownClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleDropdownClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleDropdownClickOutside);
+    };
+  }, []);
+
   return (
     <nav>
       <div className="nav-container">
@@ -15,9 +38,39 @@ const Nav = ({ handleInputChange, query }) => {
         />
       </div>
       <div className="profile-container">
-        <a href="">
-          <AiOutlineUser className="nav-icons" />
-        </a>
+        <Link to="/faq">
+          <p className="title">FAQ</p>
+        </Link>
+        <Link to="about-us">
+          <p className="title">About Us</p>
+        </Link>
+        <div ref={dropdownRef} className="relative">
+          <AiOutlineUser
+            className="cursor-pointer nav-icons"
+            onClick={handleDropdownToggle}
+          />
+          {dropdownOpen && (
+            <div className="absolute right-0 w-48 mt-2 bg-white rounded-md shadow-md">
+              <ul className="py-1 text-sm text-gray-700">
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
