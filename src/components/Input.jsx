96d1328cc0bf93dd901ae91d60d62e527752a9a5
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-04 08:33:23 +0700

    create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,21 @@
+import { PropTypes } from "prop-types";
+
+const Input = ({ handleChange, value, title, name, color }) => {
+  return (
+    <label className="sidebar-label-container">
+      <input onChange={handleChange} type="radio" value={value} name={name} />
+      <span className="checkmark" style={{ backgroundColor: color }}></span>
+      {title}
+    </label>
+  );
+};
+
+Input.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+export default Input;
