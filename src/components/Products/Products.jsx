8a78a307e9e431a38994da612e76a11fde69d023
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-07 22:07:59 +0700

    add state for opening modal detail product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import DetailProductModal from "../DetailProductModal/DetailProductModal";
 import "./Product.css";
 
 const Products = ({ result }) => {
+  const [showDetailModal, setShowDetailModal] = useState(false);
+
+  const handleModal = () => {
+    setShowDetailModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowDetailModal(false);
+  };
   return (
     <>
-      <section className="card-container">{result}</section>
+      <section onClick={handleModal} className="card-container">
+        {result}
+      </section>
+      <DetailProductModal show={showDetailModal} onClose={handleCloseModal} />;
     </>
   );
 };
