48b6dd69ccd675d02472eec53a74e01045b16510
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-07 22:48:57 +0700

    add new aliases for new route

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,7 @@
 export { default as Login } from "./auth/Login";
 export { default as SignUp } from "./auth/SignUp";
 export { default as Dashboard } from "./home/Dashboard";
+export { default as UserProfile } from "./home/UserProfile";
+export { default as Product } from "./home/Product";
+export { default as UpgradeAccount } from "./home/UpgradeAccount";
 export { default as NotFound } from "./error/NotFound";
