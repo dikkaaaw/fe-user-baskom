dd250b226ba7c77bbfca3e70b9ce479540052169
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-07 22:06:02 +0700

    add alert modal for must login first purpose

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { FaExclamationTriangle } from "react-icons/fa";
+import "./LoginModal.css";
+
+const LoginModal = ({ show, onClose }) => {
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate("/login");
+  };
+
+  if (!show) return null;
+
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <div className="alert-icon">
+          <FaExclamationTriangle size={40} />
+        </div>
+        <p>To continue you must login first! Proceed to Login page?</p>
+        <div className="modal-buttons">
+          <button className="cancel-btn" onClick={onClose}>
+            Cancel
+          </button>
+          <button
+            className="p-2 px-5 text-white rounded-md bg-slate-900 btn"
+            onClick={onLogin}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+LoginModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onLogin: PropTypes.func.isRequired,
+};
+
+export default LoginModal;
