6ca9550dac0e6bd61d6becfad680cbe5de614cdc
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-08 02:44:16 +0700

    change sidebar style

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,14 +8,33 @@ import "./Sidebar.css";
 const Sidebar = ({ handleChange }) => {
   return (
     <>
-      <section className="sidebar">
-        <div className="logo-container">
+      {/* <section classNameName="sidebar">
+        <div classNameName="logo-container">
           <Link to="/home">
-            <img src={imgLogo} alt="logo" className="logo" />
+            <img src={imgLogo} alt="logo" classNameName="logo" />
           </Link>
         </div>
         <Category handleChange={handleChange} />
         <Price handleChange={handleChange} />
+      </section> */}
+
+      {/* <!-- Component Start --> */}
+      <section className="sidebar">
+        <div className="flex flex-col items-center w-full h-full overflow-hidden text-gray-700 bg-white rounded">
+          <a className="flex items-center w-full px-3 mt-3" href="/home">
+            <div className="logo-container">
+              <Link to="/home">
+                <img src={imgLogo} alt="logo" className="logo" />
+              </Link>
+            </div>
+          </a>
+          <div className="w-full px-6">
+            <div className="w-full mt-3">
+              <Category handleChange={handleChange} />
+              <Price handleChange={handleChange} />
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
