178df7e6141a4d9af38e9000177ecc4bc14612cb
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-10 11:43:14 +0700

    add new aliases

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,6 @@ export { default as Login } from "./auth/Login";
 export { default as SignUp } from "./auth/SignUp";
 export { default as Dashboard } from "./home/Dashboard";
 export { default as UserProfile } from "./home/UserProfile";
-export { default as Product } from "./home/Product";
-export { default as UpgradeAccount } from "./home/UpgradeAccount";
+export { default as ProductManagement } from "./home/ProductManagement";
+export { default as AboutUs } from "./home/AboutUs";
 export { default as NotFound } from "./error/NotFound";
