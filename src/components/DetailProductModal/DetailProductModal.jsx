4280ae05e881237e0150ae3a90e80ed7f6f3be4c
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-11 12:33:00 +0700

    chore: Update product detail modal styling and content

--- src/components/DetailProductModal/DetailProductModal.jsx
+++ src/components/DetailProductModal/DetailProductModal.jsx
@@ -6,6 +6,7 @@ const DetailProductModal = ({ show, onClose }) => {
     name: "Dummy Product",
     description: "This is a dummy product",
     price: 10.99,
+    condition: "New",
     stock: 50,
     image: "https://via.placeholder.com/150",
   };
@@ -23,11 +24,29 @@ const DetailProductModal = ({ show, onClose }) => {
               <div className="product-image">
                 <img src={dummyProduct.image} alt={dummyProduct.name} />
               </div>
-              <div className="product-info">
-                <h2>{dummyProduct.name}</h2>
-                <p>{dummyProduct.description}</p>
-                <p>Price: {dummyProduct.price}</p>
-                <p>Stock: {dummyProduct.stock}</p>
+              <div className="ml-6 product-info">
+                <h2 className="text-3xl">{dummyProduct.name}</h2>
+                <p className="mt-6 product-description pe-6">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repellat ipsam unde obcaecati quam qui aut cupiditate nesciunt
+                  asperiores quibusdam eaque? Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Vero, rem?
+                </p>
+                <div className="flex justify-between mt-10 pe-6">
+                  <p>Price </p>
+                  {dummyProduct.price}
+                </div>
+                <div className="flex justify-between pe-6">
+                  <p>Condition</p>
+                  {dummyProduct.condition}
+                </div>
+                <div className="flex justify-between pe-6">
+                  <p>Stock</p>
+                  {dummyProduct.stock}
+                </div>
+                <button className="mt-10 add-to-cart-btn">
+                  Chat dengan penjual
+                </button>
               </div>
             </div>
           </div>
