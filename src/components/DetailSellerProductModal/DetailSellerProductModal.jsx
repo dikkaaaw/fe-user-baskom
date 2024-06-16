e4f8ea075796ecd4a2794b72d08c6b615065a01f
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-16 16:17:46 +0700

    Refactor DetailSellerProductModal component to improve UI and add product details

--- src/components/DetailSellerProductModal/DetailSellerProductModal.jsx
+++ src/components/DetailSellerProductModal/DetailSellerProductModal.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const DetailSellerProductModal = ({ show, onClose, product }) => {
-  if (!show) return null;
+  if (!show || !product) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
@@ -16,27 +16,38 @@ const DetailSellerProductModal = ({ show, onClose, product }) => {
           <div className="w-1/2 p-4">
             <img
               src={product.image || "https://via.placeholder.com/150"}
-              alt="Product Image"
+              alt="product Image"
               className="object-cover w-full h-auto rounded-lg"
             />
           </div>
           <div className="w-1/2 p-4">
             <h2 className="mb-4 text-2xl font-bold">{product.name}</h2>
-            <p className="mb-2">
-              <strong>Price:</strong> ${product.price}
-            </p>
-            <p className="mb-2">
-              <strong>Description:</strong> {product.description}
-            </p>
-            <p className="mb-2">
-              <strong>Category:</strong> {product.category}
-            </p>
-            <button
-              className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-700"
-              onClick={onClose}
-            >
-              Close
-            </button>
+            <div className="flex justify-between">
+              <p className="mb-2">
+                <strong>Price</strong>
+              </p>
+              <p>${product.price}</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="mb-2">
+                <strong>Stock</strong>
+              </p>
+              <p>{product.qty}</p>
+            </div>
+            <div className="flex justify-between mb-2">
+              <p>
+                <strong>Category</strong>
+              </p>
+              <p>
+                {product.categories.map((category) => category.name).join(", ")}
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <p className="mb-2">
+                <strong>Description</strong>
+              </p>
+              <p>{product.description}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -50,10 +61,16 @@ DetailSellerProductModal.propTypes = {
   product: PropTypes.shape({
     name: PropTypes.string,
     price: PropTypes.number,
+    qty: PropTypes.number,
     description: PropTypes.string,
     category: PropTypes.string,
     image: PropTypes.string,
-  }).isRequired,
+    categories: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+      })
+    ),
+  }),
 };
 
 export default DetailSellerProductModal;
