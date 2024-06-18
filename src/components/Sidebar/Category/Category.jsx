29715d2cabcb179778dbcace452d8a325d1e61e2
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-19 00:39:28 +0700

    chore: Update Category component to handle category changes and display dynamic inputs

--- src/components/Sidebar/Category/Category.jsx
+++ src/components/Sidebar/Category/Category.jsx
@@ -1,41 +1,30 @@
-import { PropTypes } from "prop-types";
+import PropTypes from "prop-types";
 import Input from "../../Input";
 import "./Category.css";
 
-function Category({ handleChange }) {
+function Category({ handleChange, categories }) {
   return (
     <div>
-      <h2 className="sidebar-title ms-4">Category</h2>
-
+      <h2 className="sidebar-title ms-4">Kategori</h2>
       <div className="ms-4">
         <label className="sidebar-label-container">
-          <input onChange={handleChange} type="radio" value="" name="test" />
+          <input
+            onChange={handleChange}
+            type="radio"
+            value=""
+            name="category"
+          />
           <span className="checkmark"></span>All
         </label>
-        <Input
-          handleChange={handleChange}
-          value="sneakers"
-          title="Sneakers"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="flats"
-          title="Flats"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="sandals"
-          title="Sandals"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="heels"
-          title="Heels"
-          name="test"
-        />
+        {categories.map((category) => (
+          <Input
+            key={category.id}
+            handleChange={handleChange}
+            value={category.name}
+            title={category.name}
+            name="category"
+          />
+        ))}
       </div>
     </div>
   );
@@ -43,6 +32,7 @@ function Category({ handleChange }) {
 
 Category.propTypes = {
   handleChange: PropTypes.func.isRequired,
+  categories: PropTypes.array.isRequired,
 };
 
 export default Category;
