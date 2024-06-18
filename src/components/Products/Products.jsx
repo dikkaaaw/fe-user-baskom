e10294615dff28818422f3f4390e1cd7d5b9838f
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-19 01:57:16 +0700

    chore: Improve card rendering in Products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -19,41 +19,52 @@ const Products = ({ result }) => {
 
   return (
     <>
-      <section className="card-container">
-        {result.map((product) => (
-          <div
-            key={product.id}
-            className="card"
-            onClick={() => handleModal(product.id)}
-          >
-            <img src={product.image} alt={product.name} className="card-img" />
-            <div className="card-details">
-              <h3 className="card-title">
-                <strong>{product.name}</strong>
-              </h3>
-              <div className="flex justify-between">
-                <p>Harga</p>
-                <div className="font-semibold">Rp. {product.price}</div>
-              </div>
-              <div className="flex justify-between">
-                <p>Stock</p>
-                <div className="font-semibold">{product.qty}</div>
-              </div>
-              <div className="flex flex-col">
-                <p className="mt-2">Kategori</p>
-                <div className="px-2 mt-1 rounded-xl bg-slate-200">
-                  {" "}
-                  <i>
-                    {product.categories
-                      .map((category) => category.name)
-                      .join(", ")}
-                  </i>
+      {result.length > 0 ? (
+        <section className="card-container">
+          {result.map((product) => (
+            <div
+              key={product.id}
+              className="card"
+              onClick={() => handleModal(product.id)}
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className="card-img"
+              />
+              <div className="card-details">
+                <h3 className="card-title">
+                  <strong>{product.name}</strong>
+                </h3>
+                <div className="flex justify-between">
+                  <p>Harga</p>
+                  <div className="font-semibold">Rp. {product.price}</div>
+                </div>
+                <div className="flex justify-between">
+                  <p>Stock</p>
+                  <div className="font-semibold">{product.qty}</div>
+                </div>
+                <div className="flex flex-col">
+                  <p className="mt-2">Kategori</p>
+                  <div className="px-2 mt-1 rounded-xl bg-slate-200">
+                    {" "}
+                    <i>
+                      {product.categories
+                        .map((category) => category.name)
+                        .join(", ")}
+                    </i>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
+      ) : (
+        <div className="product-not-found">
+          {" "}
+          <i>Product not found..</i>
+        </div>
+      )}
       <DetailProductModal
         show={showDetailModal}
         onClose={handleCloseModal}
