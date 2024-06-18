fc5af6d2ed6ae6a770abaa8aa9417e635ba715f3
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-19 00:39:15 +0700

    feat: Add click functionality to product cards in Products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,20 +5,60 @@ import "./Product.css";
 
 const Products = ({ result }) => {
   const [showDetailModal, setShowDetailModal] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState(null);
 
-  const handleModal = () => {
+  const handleModal = (productId) => {
+    setSelectedProductId(productId);
     setShowDetailModal(true);
   };
 
   const handleCloseModal = () => {
     setShowDetailModal(false);
+    setSelectedProductId(null);
   };
+
   return (
     <>
-      <section onClick={handleModal} className="card-container">
-        {result}
+      <section className="card-container">
+        {result.map((product) => (
+          <div
+            key={product.id}
+            className="card"
+            onClick={() => handleModal(product.id)}
+          >
+            <img src={product.image} alt={product.name} className="card-img" />
+            <div className="card-details">
+              <h3 className="card-title">
+                <strong>{product.name}</strong>
+              </h3>
+              <div className="flex justify-between">
+                <p>Harga</p>
+                <div className="font-semibold">Rp. {product.price}</div>
+              </div>
+              <div className="flex justify-between">
+                <p>Stock</p>
+                <div className="font-semibold">{product.qty}</div>
+              </div>
+              <div className="flex flex-col">
+                <p className="mt-2">Kategori</p>
+                <div className="px-2 mt-1 rounded-xl bg-slate-200">
+                  {" "}
+                  <i>
+                    {product.categories
+                      .map((category) => category.name)
+                      .join(", ")}
+                  </i>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </section>
-      <DetailProductModal show={showDetailModal} onClose={handleCloseModal} />;
+      <DetailProductModal
+        show={showDetailModal}
+        onClose={handleCloseModal}
+        productId={selectedProductId}
+      />
     </>
   );
 };
