09d19f5c7817016eef00deb8a439686916b69c74
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-19 00:39:34 +0700

    chore: Update Price component to display prices in Indonesian Rupiah (Rp)

--- src/components/Sidebar/Price/Price.jsx
+++ src/components/Sidebar/Price/Price.jsx
@@ -1,53 +1,52 @@
-import { PropTypes } from "prop-types";
+import PropTypes from "prop-types";
 import Input from "../../Input";
 import "./Price.css";
 
 const Price = ({ handleChange }) => {
   return (
-    <>
-      <div className="ms-4">
-        <h2 className="sidebar-title price-title">Price</h2>
-
-        <label className="sidebar-label-container">
-          <input
-            onChange={handleChange}
-            className="radio"
-            type="radio"
-            value=""
-            name="test2"
-          />
-          <span className="checkmark"></span>All
-        </label>
-
-        <Input
-          handleChange={handleChange}
-          value={"50"}
-          title="$0 - 50"
-          name="test2"
+    <div className="ms-4">
+      <h2 className="sidebar-title price-title">Harga</h2>
+      <label className="sidebar-label-container">
+        <input
+          onChange={handleChange}
+          className="radio"
+          type="radio"
+          value=""
+          name="price"
         />
-
-        <Input
-          handleChange={handleChange}
-          value={"100"}
-          title="$50 - $100"
-          name="test2"
-        />
-
-        <Input
-          handleChange={handleChange}
-          value={"150"}
-          title="$100 - $150"
-          name="test2"
-        />
-
-        <Input
-          handleChange={handleChange}
-          value={"200"}
-          title="Over $150"
-          name="test2"
-        />
-      </div>
-    </>
+        <span className="checkmark"></span>All
+      </label>
+      <Input
+        handleChange={handleChange}
+        value="50"
+        title="Rp0 - 50 ribu"
+        name="price"
+      />
+      <Input
+        handleChange={handleChange}
+        value="100"
+        title="Rp50 ribu - 100 ribu"
+        name="price"
+      />
+      <Input
+        handleChange={handleChange}
+        value="150"
+        title="Rp100 ribu - 150 ribu"
+        name="price"
+      />
+      <Input
+        handleChange={handleChange}
+        value="200"
+        title="Rp150 ribu - 200 ribu"
+        name="price"
+      />
+      <Input
+        handleChange={handleChange}
+        value="200+"
+        title="> Rp200 ribu"
+        name="price"
+      />
+    </div>
   );
 };
 
