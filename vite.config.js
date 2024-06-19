2819ef11ac610d74e85b05dc305421ea76579fbe
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-19 13:24:51 +0700

    chore: Update vite.config.js to remove commented out proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
+  server: {
+    // proxy: {
+    //   "/api": {
+    //     target: "https://baskom-api.up.railway.app",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+    //   },
+    // },
   },
 });
