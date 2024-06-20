556715f89ccc78bf8726c2e8e8650c1b228c3cdf
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-20 15:11:09 +0700

    chore: Add overflow-hidden class to body when DeleteModal is shown

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,26 +1,39 @@
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 
 const DeleteModal = ({ show, onClose, onDelete }) => {
+  useEffect(() => {
+    if (show) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [show]);
+
   if (!show) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
       <div className="relative w-full max-w-md p-6 mx-auto bg-white rounded-lg shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold">Confirm Deletion</h2>
+        <h2 className="text-2xl font-semibold ">Confirm Deletion</h2>
         <p className="mb-6 text-gray-600">
           Are you sure you want to delete this product?
         </p>
         <div className="flex justify-end gap-2">
           <button
             type="button"
-            className="px-4 py-2 text-white bg-gray-500 rounded"
+            className="px-4 py-2 text-sm text-white bg-gray-500 rounded"
             onClick={onClose}
           >
             Cancel
           </button>
           <button
             type="button"
-            className="px-4 py-2 text-white bg-red-500 rounded"
+            className="px-4 py-2 text-sm text-white bg-red-500 rounded"
             onClick={onDelete}
           >
             Delete
